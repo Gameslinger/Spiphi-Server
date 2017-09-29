b03f72563956f049ddcbbbdd224d05f9372b1974
Bennett.DenBleyker <Bennett.DenBleyker@DRA2-209-13.win-domain.apaserver.org>
2017-09-29 14:11:26 -0600

    P2P startup

--- src/p2pserver/P2PServer.java
+++ src/p2pserver/P2PServer.java
@@ -15,7 +15,7 @@ public class P2PServer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
