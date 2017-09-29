1aa0196c5675758deaac3cd7902b9be0b0bc727d
Gabriel.Maxfield <Gabriel.Maxfield@DRA2-209-20.win-domain.apaserver.org>
2017-09-29 14:19:18 -0600

    Refactored

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package p2pserver;
+package Main;
 
 /**
  *
  * @author Bennett.DenBleyker
  */
-public class P2PServer {
+public class Main {
 
     /**
      * @param args the command line arguments
